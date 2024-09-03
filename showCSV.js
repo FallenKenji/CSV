aa20e1576686904cc1d2e8f95af917f9d814e6f6
Kaiser <cleitinyeagerrumbling4@gmail.com>
2024-09-03 15:41:20 -0300

    Importa biblioteca para função principal

--- showCSV.js
+++ showCSV.js
@@ -2,18 +2,17 @@
 const fs = require('fs');
 const csv = fs.readFileSync('./oscar_best_pictures.csv').toString();
 const {
-    quebraLinhas, separaColunas, separaConteudo, parteDificil
-} = require ('./node_modules/csv');
-
+    quebraLinhas, separaColunas, separaConteudo, constroiObjeto, constroiListaDeObjetos
+} = require('csv')
 const linhas = quebraLinhas(csv);
 const primeiraLinha = linhas.shift();
 const headers = separaColunas(primeiraLinha);
 const conteudo = separaConteudo(linhas);
-const json = parteDificil(headers, conteudo);
 
 console.log(headers);
 console.log(conteudo);
+const json = constroiListaDeObjetos(headers, conteudo);
+
 console.table(json, ["Filme", "Ano", "Gênero"]);
 
 /////////////////////////////
-
